8af8bf9d55bae62591767c3ed63352d2280053fb
Flotroniks <florianvdc56@gmail.com>
2022-02-02 12:46:49 +0100

    music.py modif

---
+++ music.py
@@ -0,0 +1,29 @@
+
+
+
+def play():
+    print("play")
+
+
+def pause():
+    print("pause")
+
+def stop():
+    print("stop")
+
+def next():
+    print("next")
+def skip():
+    next()
+def previous():
+    print("previous")
+def repeat():
+    print("repeat")
+def random():
+    print("random")
+def ping():
+    print("ping")
+def now():
+    print("now")
+def help():
+    print("help")
