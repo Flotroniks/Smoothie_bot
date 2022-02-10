214b7540b096cf4b9e75ef52372d8329b4bd984c
Flotroniks <florianvdc56@gmail.com>
2022-02-10 20:41:17 +0100

    gitignore modif

--- score.py
+++ score.py
@@ -2,7 +2,7 @@ import requests,string,datetime,Utils,json
 
 def rootme():
     d = datetime.datetime.now()
-    day=d.strftime("%H%d")
+    day=d.strftime("%H%M%y")
     with open('day.tmp') as f:
         contents = f.read()
     if(day!=contents):
@@ -47,40 +47,43 @@ def rootme():
     
             
     
-    #tab = Utils.normalize_username(tab, Utils.max_username_length(tab))
-    
+    tab=Utils.tri_rapide(tab)  #tri du tableau
+    max = len(str(tab[0][1]))
     tmp_str=""
     for i in range(0,len(tab)):        
-        tmp_str+=" {0} Pts          {1}\n".format(tab[i][1],tab[i][0]) 
+        tmp_str+=" {0} Pts {1}\n".format(str(tab[i][1]).rjust(max),tab[i][0]) 
     
 
-
+    
     return tmp_str
 
 
 def cryptohack():
-        f=open("Usernames_cryptohack.txt", "r")
-        Users=f.readlines()
-        Users=[x.strip() for x in Users]
-        tab=[]     
-        for user in Users:
-            r=requests.get('https://cryptohack.org/api/user/'+user+'/')
-            reponse=r.json() 
-            if "username" in reponse:
-                
-             
-                tab.append([reponse["username"],int(reponse["score"]),reponse["rank"]])
+    f=open("Usernames_cryptohack.txt", "r")
+    Users=f.readlines()
+    f.close()
+    Users=[x.strip() for x in Users]
+    tab=[]     
+    for user in Users:
+        r=requests.get('https://cryptohack.org/api/user/'+user+'/')
+        reponse=r.json() 
+        if "username" in reponse:
+            
         
+            tab.append([reponse["username"],int(reponse["score"]),reponse["rank"]])
+
+
+    tab=Utils.tri_rapide(tab)  #tri du tableau
+    tmp_str=""
+    rang_max=len(str(tab[len(tab)-1][2]))
+    max_username_length=Utils.max_username_length(tab)
+    score_max=len(str(tab[0][2]))
+    #print(rang_max)
+    for i in range(0,len(tab)):        
+        tmp_str+="Rangs {0} : {1} ({2}pts) \n".format(str(tab[i][2]).rjust(rang_max),tab[i][0].ljust(max_username_length),tab[i][1])#affichage du tableau
         
-        tab=Utils.tri_rapide(tab)  #tri du tableau
-        tmp_str=""
-        rang_max=len(str(tab[len(tab)-1][2]))
-        max_username_length=Utils.max_username_length(tab)
-        score_max=len(str(tab[0][2]))
-        #print(rang_max)
-        for i in range(0,len(tab)):        
-            tmp_str+="Rangs {0} : {1} ({2}pts) \n".format(str(tab[i][2]).rjust(rang_max),tab[i][0].ljust(max_username_length),tab[i][1])#affichage du tableau
-        
-        return tmp_str
-cryptohack()
+    return tmp_str
+
+
+rootme()
 
